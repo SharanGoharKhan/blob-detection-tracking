53394a05ec3c61a9f0d0518ccd3e2267b4cea44c
Sharan Khan <sharan.gohar@gmail.com>
2017-08-06 18:38:19 +0500

    package for webcam

--- source_code/source_code/Form1.Designer.cs
+++ source_code/source_code/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace source_code
+﻿namespace Onvif.IP.Camera.Viewer
 {
     partial class Form1
     {
@@ -6,7 +6,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+ 
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -19,21 +19,55 @@
             }
             base.Dispose(disposing);
         }
-
+ 
         #region Windows Form Designer generated code
-
+ 
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(10, 230);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 23);
+            this.connectBtn.TabIndex = 2;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 210);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(274, 264);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IP Camera\'s Live Image";
+            this.ResumeLayout(false);
+ 
         }
-
+ 
         #endregion
+ 
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
